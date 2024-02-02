e86acf8cd30683ed53d9e1e5759d9e18b2186786
Alamgir Hassan <alamgir.hassan@uworx.co.uk>
2024-02-02 16:06:14 +0500

    Created customer registeration and approval components

---
+++ bank-app-frontend/Models/Applicant.cs
@@ -0,0 +1,24 @@
+﻿namespace Models
+{
+    public enum AccountType
+    {
+        CURRENT, SAVING, BUSINESS
+    }
+    public enum AccountStatus
+    {
+        APPROVED, DENIED, PENDING
+    }
+    public class Applicant
+    {
+        public long Id { get; set; }
+        public string ApplicateName { get; set; }
+        public string Address { get; set; }
+        public string CNIC { get; set; }
+        public DateTime DOB { get; set; } // Change DateOnly to DateTime
+        public AccountType AccountType { get; set; }
+        public AccountStatus AccountStatus { get; set; } = AccountStatus.PENDING;
+        public long? TellerId { get; set; }
+        public Teller Teller { get; set; } // Assuming TellerViewModel is another ViewModel
+        public Customer Customer { get; set; } 
+    }
+}
---
+++ bank-app-frontend/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public class Customer
+    {
+        public long CustomerId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public long ApplicantId { get; set; }
+    }
+}
